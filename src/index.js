b8938fbb66c2a78b3ea5437b548cff67ab71119c
luqi <luqi@xiaoshouyi.com>
2018-08-03 10:28:29 +0800

    configure webpack and git configuration-files.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ImageScreenshot from './ImageScreenshot';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ImageScreenshot />, document.getElementById('root'));
 registerServiceWorker();
