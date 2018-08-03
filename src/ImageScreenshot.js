e74d73a1bab35cbfd63afcc84346575a2b075a9f
kairi1227 <kairi1227@126.com>
2018-08-03 14:35:52 +0800

    update index file

--- src/ImageScreenshot.js
+++ src/ImageScreenshot.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react';
+import React, {PureComponent, Fragment} from 'react';
+import PropTypes from 'prop-types';
 import './ImageScreenshot.css';
+import invariant from 'invariant';
+// import ReactCanvas from 'react-canvas';
 
-class ImageScreenshot extends Component {
+class ImageScreenshot extends PureComponent {
+  constructor(props) {
+    super(props);
+    invariant(props.image, 'The parameter image is required, it is a Image Object, eg: new Image().');
+  }
+
+  componentDidMount() {
+    const {image} = this.props;
+    if(this.canvas) {
+      const content = this.canvas.getContext('2d');
+      image.onload = () => {
+        content.drawImage(image, 0, 0);
+      }
+    }
+  }
+
   render() {
+    const {width, height} = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        {/*<ReactCanvas.Surface width={width} height={height}>*/}
+          {/*/!*<ReactCanvas.Image src={require('./webwxgetmsgimg.jpg')}/>*!/*/}
+        {/*</ReactCanvas.Surface>*/}
+        <canvas width={width} height={height} ref={r => this.canvas = r}></canvas>
+      </Fragment>
     );
   }
 }
+
+ImageScreenshot.propTypes = {
+  image: PropTypes.object.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number
+};
+
+ImageScreenshot.defaultProps = {
+  image: null,
+  width: 1024,
+  height: 768
+};
+
 
 export default ImageScreenshot;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ImageScreenshot from './ImageScreenshot';
 import registerServiceWorker from './registerServiceWorker';
+const image = new Image();
+image.src = require('./webwxgetmsgimg.jpg');
 
-ReactDOM.render(<ImageScreenshot />, document.getElementById('root'));
+ReactDOM.render(<ImageScreenshot image={image}/>, document.getElementById('root'));
 registerServiceWorker();
