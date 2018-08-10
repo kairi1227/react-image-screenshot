eda47a4f609ec7ba359841fd497593ad4f9d5f5c
kairi1227 <kairi1227@126.com>
2018-08-10 11:31:36 +0800

    show tool bar by icon font. fixed clear rect can be moved in out clear area.

--- src/ImageScreenshot.js
+++ src/ImageScreenshot.js
@@ -1,6 +1,7 @@
-import React, {PureComponent} from 'react';
+import React, {PureComponent, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import './ImageScreenshot.css';
+import './font/iconfont.css';
 import invariant from 'invariant';
 import {Stage, Layer, Image, Rect} from 'react-konva';
 import ClearReact from './ClearRect';
@@ -28,43 +29,67 @@ class ImageScreenshot extends PureComponent {
   }
 
   render() {
-    const {width, height, x, y} = this.props;
+    const {width, height, x, y, toolClassName} = this.props;
     const {init, model, clear} = this.state;
     return (
-      <Stage
-        width={width}
-        height={height}
-        onMouseup={() => {
-          if (!model && !init) {
-            this.setState({model: true});
-          }
-        }}
-        onMousedown={e => {
-          if(!model) {
-            this.setState({init: false, clear: {x: e.evt.clientX, y: e.evt.clientY}});
-          }
-        }}
-        onMousemove={e => {
-          if (!model && !init) {
-            this.setState({clear: {...clear, width: e.evt.clientX - clear.x, height: e.evt.clientY - clear.y}});
-          }
-        }}>
-        <Layer>
-          <Image image={this.state.image} x={x} y={y} width={width} height={height}/>
-          {init && <Rect strokeWidth={8} stroke={'#00AEFF'} x={x} y={x} width={width} height={height}/>}
-        </Layer>
-        <Layer>
-          {!init && <Rect fill={'#000'} opacity={0.7} sceneFunc={context => {
-            context.rect(x, y, width, height);
-            context.fill();
-            context.clearRect(clear.x, clear.y, clear.width, clear.height);
-          }}/>}
-          {
-            !init && (clear.width > 0 || clear.height > 0)
-            && <ClearReact clear={clear} changeClear={e => this.setState({clear: {...clear, ...e}})}/>
-          }
-        </Layer>
-      </Stage>
+      <Fragment>
+        <Stage
+          width={width}
+          height={height}
+          onMouseup={() => {
+            if (!model && !init) {
+              let state = {model: true};
+              if (!clear.width || !clear.height) {
+                state.clear = {x, y, width, height};
+              }
+              this.setState(state);
+            }
+          }}
+          onMousedown={e => {
+            if (!model) {
+              this.setState({init: false, clear: {...clear, x: e.evt.clientX, y: e.evt.clientY}});
+            }
+          }}
+          onMousemove={e => {
+            if (!model && !init) {
+              this.setState({clear: {...clear, width: e.evt.clientX - clear.x, height: e.evt.clientY - clear.y}});
+            }
+          }}>
+          <Layer>
+            <Image image={this.state.image} x={x} y={y} width={width} height={height}/>
+            {init && <Rect strokeWidth={8} stroke={'#00AEFF'} x={x} y={x} width={width} height={height}/>}
+          </Layer>
+          <Layer>
+            {!init && <Rect fill={'#000'} opacity={0.7} sceneFunc={context => {
+              context.rect(x, y, width, height);
+              context.fill();
+              context.clearRect(clear.x, clear.y, clear.width, clear.height);
+            }}/>}
+          </Layer>
+          <Layer>
+            {
+              !init && (clear.width > 0 || clear.height > 0)
+              && <ClearReact clear={clear} changeClear={e => this.setState({clear: {...clear, ...e}})}/>
+            }
+          </Layer>
+        </Stage>
+        {
+          !init && (clear.width > 0 || clear.height > 0)
+          &&
+          <div className={`tool-bar ${toolClassName}`}>
+            <div className={'tool-item'}><i className={'iconfont icon-rect'}/></div>
+            <div className={'tool-item'}><i className={'iconfont icon-round'}/></div>
+            <div className={'tool-item'}><i className={'iconfont icon-arrow'}/></div>
+            <div className={'tool-item'}><i className={'iconfont icon-world'}/></div>
+            |
+            <div className={'tool-item'}><i className={'iconfont icon-back'}/></div>
+            <div className={'tool-item'}><i className={'iconfont icon-save'}/></div>
+            |
+            <div className={'tool-item'}><i className={'iconfont icon-close'}/></div>
+            <div className={'tool-item'}><i className={'iconfont icon-ok'}/></div>
+          </div>
+        }
+      </Fragment>
     );
   }
 }
@@ -74,7 +99,8 @@ ImageScreenshot.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   x: PropTypes.number,
-  y: PropTypes.number
+  y: PropTypes.number,
+  toolClassName: PropTypes.string
 };
 
 ImageScreenshot.defaultProps = {
@@ -82,7 +108,8 @@ ImageScreenshot.defaultProps = {
   width: 1024,
   height: 768,
   x: 0,
-  y: 0
+  y: 0,
+  toolClassName: ''
 };
 
 
